df5ba4d902f69f7a3b0c024709ac55894de1e041
mmitch2958 <mikescottmitchell@outlook.com>
2020-10-02 12:32:45 -0400

    updating server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+server.use(logger);
+server.use('/api/project', projectRouter)
+server.use('/api/action', actionRouter)
 
 server.get('/', (req, res) => {
     res.status(200).json({
