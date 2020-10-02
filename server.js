bce7d323067ab8b1dc7790d9b675df6700dd566c
mmitch2958 <mikescottmitchell@outlook.com>
2020-10-02 12:42:07 -0400

    finsihed server.js

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const express = require('express')
 const projectRouter = require('./data/routers/projectRouter')
 const actionRouter = require('./data/routers/actionRouter')
 const helmet = require('helmet')
-
+const morgan = require('morgan');
 const server = express();
+
+const logger = morgan('combined')
+//Global Middleware 
+function greeter(req, res, next) {
+    console.log('hello')
+    next();
+} 
+  
 
 server.use(express.json());
 server.use(helmet());
 
 server.use(logger);
+server.use(greeter);
+
+// EndPoints 
 server.use('/api/project', projectRouter)
 server.use('/api/action', actionRouter)
 
@@ -19,5 +30,8 @@ server.get('/', (req, res) => {
     })
 })
 
-
+server.use((error, req, res, next) => { 
+    res.status(error.code).json({message: "Error:", error})
+    });
+  
 module.exports = server;
